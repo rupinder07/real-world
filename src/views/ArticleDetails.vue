1924e2e4af6e26bdda6a91c116e8ff43429de387
Rupinder Singh <rupinder.singh@nagarro.com>
2018-12-18 17:33:10 +0530

    Added comments

--- src/views/ArticleDetails.vue
+++ src/views/ArticleDetails.vue
@@ -1,25 +1,36 @@
 <template>
     <div class="container">
-        <div class="row">
             <div class="banner">
-            <h1>{{article.title}}</h1>
-            <User :username='username' :updatedAt='updatedAt'/>
+                <h1>{{article.title}}</h1>
+                <User :username='username' :updatedAt='updatedAt'/>
             </div>
-        </div>
-        <div class="row">
             {{article.description}}
-        </div>
-        
-    
+            <User :username='username' :updatedAt='updatedAt'/>
+            <textarea class="form-control" rows="5" id="comment" v-model="comment" placeholder="Write a comment..."></textarea>
+            <button type="submit" class="btn btn-success align-right"  @click="addComment">Post Comment</button>
+            <br/>
+            <br/>
+            <h3 class="align-center"> Comments </h3>
+            <ul class="list-group" v-for="c in commentList" :key="c">
+                <li class="list-group-item">c</li>
+            </ul>
     </div>
 </template>
 
 <script>
 
 import User from '@/components/User'
+import AxiosService from '@/services/baseaxiosservice'
 
 export default {
+
     name: 'ArticleDetails',
+    data() {
+        return {
+            comment: null,
+            commentList: []
+        }
+    },
     props: [
         'article',
         'username',
@@ -27,7 +38,34 @@ export default {
     ],
     components: {
         User
-    }
+    },
+    
+    methods: {
+        addComment() {
+            let router = this.$router; 
+            let store = this.$store;
+            AxiosService.post('articles/'+ this.article.slug+'/comments',
+            {
+                "comment": {
+                    "body":this.comment
+                },
+            },
+            this.$store.getters.getUser.token)
+            .then((response) => {
+                store.dispatch('login', response.data.user)   
+                router.push({name: 'article-details', slug: this.article.slug});
+            })
+            .catch((error) => console.log(error));
+        }
+    },
+    created() {
+            AxiosService.get('articles/'+ this.article.slug+'/comments',
+            this.$store.getters.getUser.token)
+            .then((response) => {
+               this.commentList =  response.data.comments
+            })
+            .catch((error) => console.log(error)); 
+        }
 }
 </script>
 
